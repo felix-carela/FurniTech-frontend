9755d9666ff62c135b372b1a10f6c302dc9ddef1
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-10-16 15:24:57 -0400

    Adds component folder with the 6 components

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddToCartButton() {
+  return (
+    <div>
+        <h1>This is the Add to Cart Button Component.</h1>        
+    </div>
+  )
+}
+
+export default AddToCartButton
