07c476f7109e0f43518880c6940d38b26a49b384
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-10-18 16:29:25 -0400

    Incomplete landing page and marketplace

--- src/pages/MarketPlace.tsx
+++ src/pages/MarketPlace.tsx
@@ -1,31 +1,38 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import { getAllItems } from '../api/routes';
-// import Navbar from '../components/Navbar';
-// import Header from '../components/Header';
+import Item from '../components/Item';
 
-interface Props{
-    lighting?:boolean;
-    furniture?:boolean;
-    decor?:boolean;
-    linens?:boolean;
+interface Props {
+  lighting?: boolean;
+  furniture?: boolean;
+  decor?: boolean;
+  linens?: boolean;
 }
 
-const MarketPlace: React.FC<Props> = ({lighting, furniture, decor, linens}) => {
-  const [items, setItems] = useState([])
+const MarketPlace: React.FC<Props> = ({ lighting, furniture, decor, linens }) => {
+  const [items, setItems] = useState([]);
 
-  useEffect(()=> {
-    const getItems = async() => {
-      const data = await getAllItems()
-      setItems(data)
+  useEffect(() => {
+    const getItems = async () => {
+      const data = await getAllItems();
+      setItems(data['results']);
     };
-    getItems()
-  }, [])
+    getItems();
+  }, []);
+
+  console.log("In Marketplace 1st instance in Items:")
+  console.log(items)
 
   return (
     <div>
       <h1>MarketPlace</h1>
+      <div>
+        {/* {items.map((item, index) => (
+          <Item key={index} item={item} />
+        ))} */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MarketPlace;
