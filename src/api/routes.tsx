d7a8a4774ba0f487143be534db7b5e5d0a3c5725
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-10-20 16:15:24 -0400

    Implements create order functionality

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -32,3 +32,34 @@ export const signup = async ({username, email, password} : signUpParams) => {
         console.log(error)
     }
 }
+
+
+interface createOrderParams {
+    username: string;
+    order_items: { item: number; quantity: number }[];
+}
+
+
+export const createOrder = async ({ username, order_items }: createOrderParams) => {
+    try {
+        const response = await api.post('/order-create/', {
+            username,
+            order_items
+        });
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const getOrders = async (username:string) => {
+    try {
+        const response = await api.get(`orders/by-username/${username}/`);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
+
