0452a5c28e1f5784b6b616360210c89f73a10b54
GerardoAP22 <garriazapantaleon@colgate.edu>
2023-10-22 19:39:52 -0400

    fixes order of names

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ function Footer() {
         <h4>Meet the Team!</h4>
         <div className="footer-links">
           <h4 className='footer-teams'>Front-End</h4>
+          <a className='footer-names' href="https://www.linkedin.com/in/axel-witt-021b85200/">Axel Witt</a>
           <a className='footer-names' href="https://www.linkedin.com/in/devops-jasmine/">Jasmine Williams</a>
           <a className='footer-names' href="https://www.linkedin.com/in/gerardo-arriaza-pantaleon1/">Gerardo Arriaza Pantaleon</a>
-          <a className='footer-names' href="https://www.linkedin.com/in/axel-witt-021b85200/">Axel Witt</a>
           <h4 className='footer-teams'>Back-End</h4>
           <a className='footer-names' href="https://www.linkedin.com/in/guoxing-liu-912259226/">Guoxing Liu</a>
           <a className='footer-names' href="https://www.linkedin.com/in/felix-carela/">Felix Carela</a>
